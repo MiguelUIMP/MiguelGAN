8d4307fdfbb3d4ad846ffc328aaea05acd9fa614
MiguelUIMP <91897494+MiguelUIMP@users.noreply.github.com>
2022-04-29 11:11:49 +0200

    Update wgan.py

--- wgan.py
+++ wgan.py
@@ -226,6 +226,10 @@ class WGAN_trainer:
                     loss_a= -torch.mean(self.D(real_data)) + torch.mean(self.D(self.G(fake_data))) + grad_penalty
                     loss_a.backward() # compute gradients 
                     optim_discriminator.step() # move the parameters
+                    
+                    # clip the parameters. Lets put both penalty and clipping together
+                    for p in self.D.parameters():
+                        p.data.clamp_(-self.c,self.c)
 
                 # Get the components of the loss to store them 
                 loss_a_real_data=-torch.mean(self.D(real_data)).data.cpu()
