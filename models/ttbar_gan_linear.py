3f91cf6fe34bb15f3cfedd4507f2c1fc882af67c
MiguelUIMP <91897494+MiguelUIMP@users.noreply.github.com>
2022-05-09 11:55:25 +0200

    Update ttbar_gan_linear.py

--- models/ttbar_gan_linear.py
+++ models/ttbar_gan_linear.py
@@ -59,7 +59,7 @@ class ttbarGAN():
 
 
     def __init__(self, lat_space):
-        channels = 18 # canales de salida de los datos, cuantas variables obtener
+        channels = 12 # canales de salida de los datos, cuantas variables obtener
         random_noise = 100 # canales de ruido aleatorio que meter
         self.G=DenseNNgenerator(channels=channels, random_noise=random_noise)
         self.D=DenseNNdiscriminator(channels=channels)
