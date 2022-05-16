b32c90852da915d36dde6b06b0b43f79ccb2583c
MiguelUIMP <91897494+MiguelUIMP@users.noreply.github.com>
2022-05-16 13:03:44 +0200

    Update wgan.py

--- wgan.py
+++ wgan.py
@@ -482,9 +482,9 @@ class WGAN_trainer:
 
                         plt.figure(figsize=(9.33, 7));
                         hist_range_com = (compare_df.min()[var], compare_df.max()[var])
-                        plt.hist(compare_df[var] , range=hist_range_com, bins=200, density=(plot_t=="Density"), alpha=0.5, label=f'MC simulation {data_t}; mean: {compare_mean[var]} std: {compare_std[var]}', color='cyan', alpha=0.5);
+                        plt.hist(compare_df[var] , range=hist_range_com, bins=200, density=(plot_t=="Density"), label=f'MC simulation {data_t}; mean: {compare_mean[var]} std: {compare_std[var]}', color='cyan', alpha=0.5);
                         hist_range_sam = (samples_df.min()[var], samples_df.max()[var])
-                        plt.hist(samples_df[var], range=hist_range_sam, bins=200, density=(plot_t=="Density"), alpha=0.5, label=f'Generated samples; mean: {samples_mean[var]} std: {samples_std[var]}', color='magenta', alpha=0.5);
+                        plt.hist(samples_df[var], range=hist_range_sam, bins=200, density=(plot_t=="Density"), label=f'Generated samples; mean: {samples_mean[var]} std: {samples_std[var]}', color='magenta', alpha=0.5);
                         if hist_range_sam[0]<hist_range_com[0]:
                             font = {'family': 'serif',
                                     'color':  'darkred',
