6a5e6b71991c74b8ed0796047d2505a518808eb3
MiguelUIMP <91897494+MiguelUIMP@users.noreply.github.com>
2022-06-03 11:51:35 +0200

    Update wgan.py

--- wgan.py
+++ wgan.py
@@ -529,6 +529,8 @@ class WGAN_trainer:
                         RMSE = np.round( np.sqrt( ((np.array(n_compare)-np.array(n_sample))**2).sum()/len(n_sample) ) , 2)
                         print(f'VARIABLE: {var}')
                         print(f'Root Mean Square Error (RMSE): {RMSE}')
+                        print(f'Sample mean: {samples_mean[var]} \t {data_t} mean: {compare_mean[var]}')
+                        print(f'Sample variance: {round(samples_std[var]**2,2)} \t {data_t} variance: {round(compare_std[var]**2},2)')
                         print(f'Sample skewness: {samples_skew[var]} \t {data_t} skewness: {compare_skew[var]}')
                         print(f'Sample kurtosis: {samples_kurtosis[var]} \t {data_t} kurtosis: {compare_kurtosis[var]}')
                         
@@ -600,7 +602,7 @@ class WGAN_trainer:
                         print(f'VARIABLE: {var}')
                         print(f'Root Mean Square Error (RMSE): {RMSE}')
                         print(f'Sample mean: {samples_mean[var]} \t {data_t} mean: {compare_mean[var]}')
-                        print(f'Sample variance: {samples_std[var]**2} \t {data_t} variance: {compare_std[var]**2}')
+                        print(f'Sample variance: {round(samples_std[var]**2,2)} \t {data_t} variance: {round(compare_std[var]**2},2)')
                         print(f'Sample skewness: {samples_skew[var]} \t {data_t} skewness: {compare_skew[var]}')
                         print(f'Sample kurtosis: {samples_kurtosis[var]} \t {data_t} kurtosis: {compare_kurtosis[var]}')
                         
